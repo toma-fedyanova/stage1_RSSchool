13ec3ee2c368bf31a4959f3e1e12bf337ea8412c
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-04-07 12:55:56 +1000

    fix: correct background dark

--- shelter/pets/script.js
+++ shelter/pets/script.js
@@ -2,6 +2,7 @@ let burger = document.querySelector('.burger__menu');
 let menu = document.querySelector('.header__navigation')
 let links =menu.querySelectorAll('li');
 let main = document.querySelector('main');
+let block = document.querySelector('.overlay');
 
 
 //burger menu exits on the right
@@ -9,11 +10,13 @@ burger.addEventListener('click', function(event) {  //add listener to burger but
   event.stopImmediatePropagation();
   menu.classList.toggle('appier');
   this.classList.toggle('rotate');
+  block.classList.toggle('overlay');
 })
 
 main.addEventListener('click', function() { //click for not menu 
      menu.classList.remove('appier');
-    burger.classList.remove('rotate');
+     burger.classList.remove('rotate');
+     block.classList.toggle('overlay');
 })
 const mediaQuery = window.matchMedia('(max-width: 767px)')
 if (mediaQuery.matches) {
@@ -21,6 +24,7 @@ if (mediaQuery.matches) {
     li.addEventListener('click', () => { //click on the links 
       menu.classList.remove('appier');
       burger.classList.remove('rotate');
+      block.classList.toggle('overlay');
     })
   }
 }
