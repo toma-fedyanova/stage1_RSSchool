f9f910ba915cd97f9dbd578a81cb71223c9530af
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-05-16 11:41:58 +1000

    feat: add eslintr

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        'max-len': ['error', { code: 150 }],
+         'no-undef': 'off'
+    }
+}
