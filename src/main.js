f1447ff62a51c500e8ef03530be043677c0a5f6e
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-05-16 16:10:47 +1000

    feat: add html plugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Creator from './Creator'
+import Creator from './Creator';
 // npm init @eslint/config
 // npm install eslint-plugin-import --save-dev  npm i eslint eslint-config-airbnb-base
 // npm install -D webpack webpack-cli
+// npm install -D html-webpack-plugin и подключить в конфиг класс, удалить dist и запустить npm run build
 
 getStart();
 function getStart(num = 10) {
@@ -10,3 +11,15 @@ function getStart(num = 10) {
   element.getField();
   element.setButtons();
 }
+
+function countClick() {
+  let counter = 0;
+  let field = document.getElementsByClassName('field')[0];
+  const listenerClick = () => counter++;
+  
+  field.addEventListener('click', function func(event) {
+    if (event.target.matches('button_closed')) listenerClick();
+  })
+  return counter;
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,11 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    })
+  ]
 }
