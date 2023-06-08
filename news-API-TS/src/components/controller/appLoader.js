7fa4c44aac3fc162a912ff17bef81d151e187a32
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-06-08 13:46:04 +1000

    feat: add api key

--- news-API-TS/src/components/controller/appLoader.js
+++ news-API-TS/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e16a03abb21d4a64b09b17d1ef7ad7cf', // получите свой ключ https://newsapi.org/
         });
     }
 }
