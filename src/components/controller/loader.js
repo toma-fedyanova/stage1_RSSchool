7ceed7fa92c6b09efe5a89fcf5521843bad81f37
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-06-12 21:56:16 +1000

    feat: add style for footer

--- src/components/controller/loader.js
+++ src/components/controller/loader.js
@@ -34,7 +34,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method /*:string*/, endpoint /*:string */, callback, options /*:Record<string, string> */ = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
