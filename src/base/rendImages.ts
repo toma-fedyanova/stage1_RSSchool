49abb87a22d32a86c3e5600c8f1904e8455e34be
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-06-20 22:49:50 +1000

    feat: add class for image block

--- src/base/rendImages.ts
+++ src/base/rendImages.ts
@@ -1,6 +1,6 @@
 /* import img from './image/backgrd.png';
 
-class Creator {
+class CreatorImage {
 
   createImage():void {
     const temp = new Image();
@@ -13,3 +13,24 @@ class Creator {
 }
 
 export default Creator;*/
+
+class CreatorImage {
+  changeLink(selector: string, link: string):void {
+  const element = document.querySelector(selector) as HTMLDivElement;
+  element.style.backgroundImage = link;
+  }
+
+  additionPicture(parent:HTMLDivElement, topSize: string, leftSize: string):void {
+    const element = document.createElement('div') as HTMLDivElement;
+    element.classList.add('image');
+    element.style.position = 'relative';
+    element.style.top = topSize;
+    element.style.left = leftSize;
+    parent.append(element);
+  }
+
+  cearBackground(selector:string):void {
+    const element = document.querySelector(selector) as HTMLDivElement;
+    element.style.backgroundImage = '';
+  }
+}
--- src/components/blockPicture.ts
+++ src/components/blockPicture.ts
@@ -1,14 +1,14 @@
 import { arrayString } from '../types/type';
 
 export const CodeText: arrayString = [
-  ['url(./image/dog.png)', 'url(./image/cat.png)', 'url(./image/bird.png)'],
-  ['url(./image/cat2.png)', 'url(./image/cat.png)', 'url(./image/dog3.png)'],
-  ['url(./image/bird2.png)', 'url(./image/cat2.png)', 'url(./image/dog.png)'],
-  ['url(./image/dog2.png)', 'url(./image/cat2.png)', 'url(./image/dog.png)'],
-  ['url(./image/dog3.png)', 'url(./image/cat2.png)', 'url(./image/dog2.png)'],
-  ['url(./image/dog.png)', 'url(./image/cat.png)', 'url(./image/dog2.png)'],
-  ['url(./image/dog.png)', 'url(./image/dog3.png)', 'url(./image/dog2.png)'],
-  ['url(./image/cat2.png)', 'url(./image/dog3.png)', 'url(./image/bird2.png)'],
-  ['url(./image/cat.png)', 'url(./image/dog.png)', 'url(./image/bird.png)'],
-  ['url(./image/cat.png)', 'url(./image/dog2.png)', 'url(./image/dog.png)'],
+  ['url("./image/dog.png")', 'url("./image/cat.png")', 'url("./image/bird.png")'],
+  ['url("./image/cat2.png")', 'url("./image/cat.png")', 'url("./image/dog3.png")'],
+  ['url("./image/bird2.png")', 'url("./image/cat2.png")', 'url("./image/dog.png")'],
+  ['url("./image/dog2.png")', 'url("./image/cat2.png")', 'url("./image/dog.png")'],
+  ['url("./image/dog3.png")', 'url("./image/cat2.png")', 'url("./image/dog2.png")'],
+  ['url("./image/dog.png")', 'url("./image/cat.png")', 'url("./image/dog2.png")'],
+  ['url("./image/dog.png")', 'url("./image/dog3.png")', 'url("./image/dog2.png")'],
+  ['url("./image/cat2.png")', 'url("./image/dog3.png")', 'url("./image/bird2.png")'],
+  ['url("./image/cat.png")', 'url("./image/dog.png")', 'url("./image/bird.png")'],
+  ['url("./image/cat.png")', 'url("./image/dog2.png")', 'url("./image/dog.png")'],
 ]
