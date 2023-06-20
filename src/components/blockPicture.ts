b744c11ea2efd13ae0fcaa71bfd4fe898f811ba8
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-06-20 23:08:04 +1000

    feat: add img

--- src/components/blockPicture.ts
+++ src/components/blockPicture.ts
@@ -6,9 +6,9 @@ export const CodeText: arrayString = [
   ['url("./image/bird2.png")', 'url("./image/cat2.png")', 'url("./image/dog.png")'],
   ['url("./image/dog2.png")', 'url("./image/cat2.png")', 'url("./image/dog.png")'],
   ['url("./image/dog3.png")', 'url("./image/cat2.png")', 'url("./image/dog2.png")'],
-  ['url("./image/dog.png")', 'url("./image/cat.png")', 'url("./image/dog2.png")'],
+  ['url("./image/dog4.png")', 'url("./image/cat.png")', 'url("./image/dog2.png")'],
   ['url("./image/dog.png")', 'url("./image/dog3.png")', 'url("./image/dog2.png")'],
   ['url("./image/cat2.png")', 'url("./image/dog3.png")', 'url("./image/bird2.png")'],
   ['url("./image/cat.png")', 'url("./image/dog.png")', 'url("./image/bird.png")'],
-  ['url("./image/cat.png")', 'url("./image/dog2.png")', 'url("./image/dog.png")'],
+  ['url("./image/cat.png")', 'url("./image/dog4.png")', 'url("./image/dog.png")'],
 ]
